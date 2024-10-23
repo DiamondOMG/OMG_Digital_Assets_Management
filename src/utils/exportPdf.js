ecafc53568b83b1c29ebbd02080fc9a561a315f9
Natnatee <natnatee_mond@hotmail.com>
2024-10-23 16:26:12 +0700

    thai sarabun

--- src/utils/exportPdf.js
+++ src/utils/exportPdf.js
@@ -1,15 +1,27 @@
 import { jsPDF } from "jspdf";
 import autoTable from "jspdf-autotable";
+import { font } from "@/utils/Sarabun-Regular-normal";
 
 export const exportPdf = (rows, columns) => {
 	// สร้าง PDF เป็นแนวนอน
 	const doc = new jsPDF({ orientation: "landscape" });
+
+	// เพิ่มฟอนต์ที่ต้องการใช้
+	doc.addFileToVFS("MyFont.ttf", font);
+	doc.addFont("MyFont.ttf", "MyFont", "normal");
+	doc.setFont("MyFont");
+
+	// สร้างข้อมูลตาราง
 	const tableData = rows.map((row) => Object.values(row.original));
 	const tableHeaders = columns.map((c) => c.header);
 
+	// ใช้ autoTable และกำหนดฟอนต์ใน styles
 	autoTable(doc, {
 		head: [tableHeaders],
 		body: tableData,
+		styles: {
+			font: "MyFont", // กำหนดฟอนต์สำหรับข้อความในตาราง
+		},
 	});
 
 	// เปิด PDF ในแท็บใหม่พร้อมปุ่มดาวน์โหลด
