d5b9a214ba3436fb6537f55625d71864aeca4149
neab-dev01 <omgdigital.developer.01@gmail.com>
2024-11-13 17:31:23 +0700

    dev >  login page
    -ติดตั้ง boostrap 5.3.3
    -coding UI หน้า login 70%

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,9 @@
 import { useState, useEffect } from "react";
 import Table from "@/components/table";
 import Loader from "@/components/loader";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './globals.css';
+import Link from 'next/link';
 
 const ScreensPage = () => {
 	const [data, setData] = useState(null); // สร้าง state สำหรับเก็บข้อมูล screens
@@ -52,9 +55,35 @@ const ScreensPage = () => {
 
 	// แสดงข้อมูลเมื่อโหลดสำเร็จ
 	return (
-		<div>
-			<Table data={data} columns={columns} />
-		</div>
+		<div className={`loginContainer container-fluid d-flex align-items-center justify-content-center`}>
+			<div className={`loginBox row`}>
+			{/* Left side (Login form) */}
+			<div className="col-lg-6 col-md-12 p-5">
+				<h2>Login</h2>
+				<form>
+				<div className="mb-4">
+					<label className="form-label">Username</label>
+					<input type="text" className="form-control" placeholder="username" />
+				</div>
+				<div className="mb-4">
+					<label className="form-label">Password</label>
+					<input type="password" className="form-control" placeholder="password" />
+				</div>
+				<button type="submit" className={`btn btn-primary w-100 loginButton`}>Login</button>
+				</form>
+			</div>
+	
+			{/* Right side (Welcome message) */}
+			<div className={`welcomeBox col-lg-6 col-md-12 d-flex flex-column align-items-center justify-content-center`}>
+				<div className={`mb-4 logo`}>Logo</div>
+				<h3>Welcome to Login</h3>
+				<p>Don’t have an account?</p>
+				<Link href="#" className="registerButton  btn btn-outline-light">
+				   Register
+				</Link>
+			</div>
+			</div>
+	  	</div>
 	);
 };
 
